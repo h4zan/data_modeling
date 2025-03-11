602cb1e39e067bb0db3ec17eddae7cee6b85cd35
h4zan <h4zan@hotmail.com>
2025-03-11 14:50:13 +0100

    setup tables with data

--- setup_postgres/sql/ezecream_table_setup.sql
+++ setup_postgres/sql/ezecream_table_setup.sql
@@ -10,3 +10,31 @@ CREATE TABLE IF NOT EXISTS Customer (
     email VARCHAR(255) NOT NULL,
     address VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    order_date DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customer (customer_id)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS Product(
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    price DOUBLE PRECISION NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS OrderLine(
+    orderline_id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER,
+
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    FOREIGN KEY (product_id) REFERENCES Product (product_id)
+
+);
+
